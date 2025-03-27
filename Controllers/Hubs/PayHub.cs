cefba553aacc8d72482d8fbd1cee4ce329062480
FK <FK@FARUKKALKAN>
2025-03-27 04:45:07 +0300

    Add project files.

---
+++ Controllers/Hubs/PayHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Server.Hubs;
+
+public class PayHub : Hub
+{
+    public static readonly IDictionary<string, string> TransationConnections = new Dictionary<string, string>();
+    public void RegisterTransaction(string id) 
+    {
+        var connectionId = Context.ConnectionId;
+        TransationConnections[id] = connectionId;
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        var connectionId = Context.ConnectionId;
+        var item = TransationConnections.FirstOrDefault(p => p.Value == connectionId);
+        TransationConnections.Remove(connectionId);
+        return base.OnDisconnectedAsync(exception);
+    }
+}
+
---
+++ Controllers/PaymentsController.cs
@@ -0,0 +1,169 @@
+﻿using Iyzipay;
+using Iyzipay.Model;
+using Iyzipay.Request;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Server.Controllers.Hubs;
+using Server.Hubs;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+
+        private readonly IHubContext<PayHub> _hubContext;
+
+        public PaymentsController(IHubContext<PayHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Pay()
+        {
+            Options options = new()
+            {
+                ApiKey = "sandbox-GRmVVUfc69m7Uc84uActbcPQK32nxZpy",
+                SecretKey = "sandbox-IsGVmXk4lEBjNsUi4ZkOpwAq0GHKlj4J",
+                BaseUrl = "https://sandbox-api.iyzipay.com"
+            };
+
+            try
+            {
+                // Ödeme isteği oluşturma
+                CreatePaymentRequest request = new CreatePaymentRequest();
+                request.Locale = Locale.TR.ToString();
+                request.ConversationId = Guid.NewGuid().ToString();
+                request.Price = "1";
+                request.PaidPrice = "1.2";
+                request.Currency = Currency.TRY.ToString();
+                request.Installment =1;
+                request.BasketId = "B67832";
+                request.PaymentChannel = PaymentChannel.WEB.ToString();
+                request.PaymentGroup = PaymentGroup.PRODUCT.ToString();
+                request.CallbackUrl = "https://localhost:7056/api/Payments/PayCallBack"; // 3D sonucunun döneceği URL
+
+                // Kart bilgileri
+                PaymentCard paymentCard = new PaymentCard();
+                paymentCard.CardHolderName = "John Doe";
+                paymentCard.CardNumber = "5528790000000008";
+                paymentCard.ExpireMonth = "12";
+                paymentCard.ExpireYear = "2030";
+                paymentCard.Cvc = "123";
+                paymentCard.RegisterCard = 0;
+                request.PaymentCard = paymentCard;
+
+                // Alıcı bilgileri
+                Buyer buyer = new Buyer();
+                buyer.Id = "BY789";
+                buyer.Name = "John";
+                buyer.Surname = "Doe";
+                buyer.GsmNumber = "+905350000000";
+                buyer.Email = "email@email.com";
+                buyer.IdentityNumber = "74300864791";
+                buyer.RegistrationAddress = "";
+                buyer.RegistrationDate = "";
+                buyer.RegistrationAddress = "Nidakule Göztepe,Merdivenköy Mah. Bora Sok. No:1";
+                buyer.Ip = "85.34.78.112";
+                buyer.City = "Istanbul";
+                buyer.Country = "Turkey";
+                buyer.ZipCode = "34732";
+                request.Buyer = buyer;
+
+                // Teslimat/adres bilgileri
+                Address shippingAddress = new Address();
+                shippingAddress.ContactName = "Jane Doe";
+                shippingAddress.City = "Istanbul";
+                shippingAddress.Country = "Turkey";
+                shippingAddress.Description = "Nidakule Göztepe,Merdivenköy Mah. Bora Sok. No:1";
+                shippingAddress.ZipCode = "34732";
+                request.ShippingAddress = shippingAddress;
+
+                // Fatura/adres bilgileri
+                Address billingAddress = new Address();
+                billingAddress.ContactName = "Jane Doe";
+                billingAddress.City = "Istanbul";
+                billingAddress.Country = "Turkey";
+                billingAddress.Description = "Nidakule Göztepe,Merdivenköy Mah. Bora Sok. No:1";
+                billingAddress.ZipCode = "34732";
+                request.BillingAddress = billingAddress;
+
+                // Sepet ürünleri
+                List<BasketItem> basketItems = new List<BasketItem>();
+                BasketItem firstBasketItem = new BasketItem();
+                firstBasketItem.Id = "B1101";
+                firstBasketItem.Name = "Binocular";
+                firstBasketItem.Category1 = "Collectibles";
+                firstBasketItem.Category2 = "Accessories";
+                firstBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+                firstBasketItem.Price = "0.3";
+                basketItems.Add(firstBasketItem);
+
+                BasketItem secondBasketItem = new BasketItem();
+                secondBasketItem.Id = "B1102";
+                secondBasketItem.Name = "Game code";
+                secondBasketItem.Category1 = "Game";
+                secondBasketItem.Category2 = "Online Game Items";
+                secondBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+                secondBasketItem.Price = "0.5";
+                basketItems.Add(secondBasketItem);
+
+                BasketItem thirdBasketItem = new BasketItem();
+                thirdBasketItem.Id = "B1103";
+                thirdBasketItem.Name = "Usb";
+                thirdBasketItem.Category1 = "Electronics";
+                thirdBasketItem.Category2 = "Usb / Cable";
+                thirdBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+                thirdBasketItem.Price = "0.2";
+                basketItems.Add(thirdBasketItem);
+                request.BasketItems = basketItems;
+
+
+                // 3D Secure ödeme başlatma
+                ThreedsInitialize threedsInitialize = ThreedsInitialize.Create(request, options);
+
+                return Ok(new { Content = threedsInitialize.HtmlContent,ConversationId=request.ConversationId });
+
+            }
+            catch (Exception ex)
+            {
+                // Hata yönetimi
+                Console.WriteLine($"Hata oluştu: {ex.Message}");
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PayCallBack([FromForm] IFormCollection collections)
+        {
+
+
+            CallbackData data = new(
+                Status: collections["status"],
+                PaymentId: collections["paymentId"],
+                ConversationData: collections["conversationData"],
+                ConversationId: collections["conversationId"],
+                MDStatus: collections["mDStatus"]);
+
+            if (data.Status != "success")
+            {
+                return BadRequest("Ödeme Başarısız oldu!");
+            }
+
+            await _hubContext.Clients.Client(PayHub.TransationConnections[data.ConversationId]).SendAsync("Receive", data);
+
+            return Ok();
+        }
+
+        public sealed record CallbackData(
+            string Status,
+            string PaymentId,
+            string ConversationData,
+            string ConversationId,
+            string MDStatus);
+
+    }
+}
